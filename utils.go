3476e8384df372f4da52951aef0bc363cc6669b1
Adrian-George Bostan <adi@fractor.ro>
2015-05-27 13:07:07 +0300

    Add TicksPerSecond constant obtained from sysconf(_SC_CLK_TCK) call

--- utils.go
+++ utils.go
@@ -6,11 +6,20 @@ import (
 	"strings"
 )
 
+// #include <unistd.h>
+import "C"
+
 var (
+	TicksPerSecond uint64
+
 	ErrUserNotFound      = errors.New("user not found")
 	ErrGroupNotFound     = errors.New("group not found")
 	ErrInvalidFileFormat = errors.New("invalid file format")
 )
+
+func init() {
+	TicksPerSecond = uint64(C.sysconf(C._SC_CLK_TCK))
+}
 
 func readSingleValueFile(path string) (string, error) {
 	content, err := ioutil.ReadFile(path)
